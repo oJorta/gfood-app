1a8de9537cf0757f44f803c0ca3359030f70ee4b
Joao Pedro <jpcsnoberto@gmail.com>
2023-04-07 14:54:53 -0300

    products grid turns into carousel when in mobile

--- scripts/carousel.js
+++ scripts/carousel.js
@@ -1,30 +1,79 @@
-const lastTestimonial = document.querySelector(".last-card")
-const nextTestimonial = document.querySelector(".next-card")
-let testimonialIndex = 1
+const lastCard = document.querySelectorAll(".last-card")
+const nextCard = document.querySelectorAll(".next-card")
 
-lastTestimonial.addEventListener('click', ()=>{
-    changeTestimonial(-1)
+let indexes = {
+    'testimonial': 1,
+    'product': 1
+}
+let windowWidthMobile = window.matchMedia('(max-width: 800px)')
+
+
+if(windowWidthMobile.matches){
+    showCard(indexes["product"], "product")
+}
+window.addEventListener('resize', () =>{
+    if(windowWidthMobile.matches){
+        showCard(indexes["product"], "product")
+    }
+    else{
+        restoreProductsGrid()
+    }
 })
-nextTestimonial.addEventListener('click', ()=>{
-    changeTestimonial(1)
+
+showCard(indexes["testimonial"], "testimonial")
+
+lastCard.forEach(lastCardBtn => {
+    lastCardBtn.addEventListener('click', (e)=>{
+        changeCard(-1, e.target.id)
+    });
+    
+});
+
+nextCard.forEach(nextCardBtn => {
+    nextCardBtn.addEventListener('click', (e)=>{
+        changeCard(1, e.target.id)
+    });
 })
 
-showTestimonial(testimonialIndex)
 
-function changeTestimonial(n){
-    showTestimonial(testimonialIndex += n)
+function changeCard(n, carouselName){
+    showCard(indexes[carouselName] += n, carouselName)
 }
 
-function showTestimonial(n){
+function showCard(n, carouselName){
     let testimonials = document.querySelectorAll(".testimonial-card")
-    if(n > testimonials.length){
-        testimonialIndex = 1
+    let products = document.querySelectorAll(".item")
+
+    if(carouselName === "testimonial"){
+        if(n > testimonials.length){
+            indexes[carouselName] = 1
+        }
+        if(n < 1){
+            indexes[carouselName] = testimonials.length
+        }
+        for(let i = 0; i<testimonials.length; i++){
+            testimonials[i].style.display = "none"
+        }
+        testimonials[indexes[carouselName]-1].style.display = "flex"
     }
-    if(n < 1){
-        testimonialIndex = testimonials.length
+    else{
+        if(n > products.length){
+            indexes[carouselName] = 1
+        }
+        if(n < 1){
+            indexes[carouselName] = products.length
+        }
+        for(let i = 0; i<products.length; i++){
+            products[i].style.display = "none"
+        }
+        products[indexes[carouselName]-1].style.display = "flex"
     }
-    for(let i = 0; i<testimonials.length; i++){
-        testimonials[i].style.display = "none"
+}
+
+function restoreProductsGrid(){
+    let products = document.querySelectorAll(".item")
+
+    for(let i = 0; i<products.length; i++){
+        products[i].style.display = "block"
     }
-    testimonials[testimonialIndex-1].style.display = "flex"
 }
